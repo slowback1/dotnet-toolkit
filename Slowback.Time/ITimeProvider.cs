769e5e4984556a7d6bc530aa4c8373076a8ea088
Andrew Wobeck <dwobeck@gmail.com>
2024-07-26 06:15:44 -0500

    Add a time abstraction

---
+++ Slowback.Time/ITimeProvider.cs
@@ -0,0 +1,13 @@
+﻿namespace Slowback.Time;
+
+public interface ITimeProvider
+{
+    DateTime Now();
+    DateTime Today();
+    DateTime FromString(string value);
+    DateTime UtcNow();
+    DateTime AddDays(DateTime date, int days);
+    DateTime AddHours(DateTime date, int hours);
+    DateTime AddMinutes(DateTime date, int minutes);
+    DateTime AddSeconds(DateTime date, int seconds);
+}
